30a81aec4d3fd0eec60e8aeab14685ec4e47391d
Satvikkkkk <satvik2521@gmail.com>
2024-08-19 04:45:27 +0530

    added seek bar and made it responsive

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,7 +12,7 @@ import Player from "@/components/Player";
 const font = Figtree({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Spotify Clone",
+  title: "pityfy",
   description: "Listen to music",
 };
 
